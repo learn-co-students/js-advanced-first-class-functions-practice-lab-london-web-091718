5a62b652be614baed9a3f108cb1e28114bd9db8e
deedeeh <dinahafez.8@gmail.com>
2018-10-29 18:21:18 +0000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+function logDriverNames(drivers) {
+  drivers.forEach(function(driver) {
+    console.log(driver.name);
+  });
+};
+
+function logDriversByHometown(drivers, hometown) {
+  drivers.forEach(function(driver) {
+    if(driver.hometown === hometown) {
+      console.log(driver.name);
+    }
+  });
+};
+
+function driversByRevenue(drovers) {
+  const drivers = [...drovers]
+  return drivers.sort(function(a, b) {
+    return a.revenue - b.revenue;
+  });
+};
+
+function driversByName(drovers) {
+  const drivers = [...drovers]
+  return drivers.sort(function(a, b) {
+    return a.name.localeCompare(b.name);
+  });
+};
+
+function totalRevenue(drivers) {
+  return drivers.reduce(function(acc, currentDriver) {
+    return acc + currentDriver.revenue;
+  }, 0);
+};
+
+function averageRevenue(drivers) {
+  return totalRevenue(drivers) / drivers.length;
+}
