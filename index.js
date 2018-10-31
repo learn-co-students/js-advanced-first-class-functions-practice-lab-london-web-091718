a4edc6c8cfc3167977cfc729562a353c6da24883
wimbleclaw <aisha305@hotmail.co.uk>
2018-10-31 11:13:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solution in this file!
+const logDriverNames = function(drivers) {
+  drivers.forEach(function(el) {
+    console.log(el.name)
+  });
+}
+
+const logDriversByHometown = (drivers, hometown) => {
+  drivers.forEach(function(el) {
+    if (el.hometown === hometown) {
+      console.log(el.name)
+    }
+  })
+}
+
+const driversByRevenue = (drivers) => {
+  driverArray = []
+  for (var i of drivers) {
+    driverArray.push(i);
+  }
+  return driverArray.sort(function(driver1, driver2) {
+    return driver1.revenue - driver2.revenue;
+  });
+}
+
+const driversByName = (drivers) => {
+  driverArray = []
+  for (var i of drivers) {
+    driverArray.push(i);
+  }
+  return driverArray.sort(function(driver1, driver2) {
+    return driver1.name.localeCompare(driver2.name);
+  });
+}
+
+const totalRevenue = (drivers) => {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  revenue = []
+  for (var i of drivers) {
+    revenue.push(i.revenue)
+  }
+  return revenue.reduce(reducer)
+}
+
+const averageRevenue = (drivers) => {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  revenue = []
+  driversNum = 0
+  for (var i of drivers) {
+    revenue.push(i.revenue)
+    driversNum ++
+  }
+  return (revenue.reduce(reducer) / driversNum)
+}
